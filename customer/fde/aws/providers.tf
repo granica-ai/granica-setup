d9d19eab356fdcaf1ea4556151bb84c932b5d052
granica <vishal.tripathi@granica.ai>
2025-02-07 12:10:22 -0800

    corrected the default bucket region in providers.tf

--- customer/fde/aws/providers.tf
+++ customer/fde/aws/providers.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     # These are dev defaults that can be overridden by backend.conf in production
     bucket = "kry-ci-granica-setup-terraform-state"
-    region = "us-east-2"
+    region = "us-west-2"
     key    = "dev"
   }
 
