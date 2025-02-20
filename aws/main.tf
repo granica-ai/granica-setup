9aaa170c0fc6e2165225f6648f8c5d36fa723abb
granica <vishal.tripathi@granica.ai>
2025-02-20 14:18:29 -0800

    modified the aws ami to use the latest al2023-ami-2023.*-kernel-6.1-x86_64 available image

--- aws/main.tf
+++ aws/main.tf
@@ -123,8 +123,7 @@ data "aws_ami" "al2023" {
 
   filter {
     name = "name"
-    #values = ["al2023-ami-2023*-kernel-*-x86_64"]
-    values = ["al2023-ami-2023.6.20241121.0-kernel-6.1-x86_64"]
+    values = ["al2023-ami-2023.*-kernel-6.1-x86_64"]
   }
 
   filter {
